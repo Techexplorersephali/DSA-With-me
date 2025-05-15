c2664197c751f05d3604e5b4aad36ae75bd50839
Techexplorersephali <138424988+Techexplorersephali@users.noreply.github.com>
2025-05-15 18:41:19 +0000

    algorithms

--- c++STL.cpp
+++ c++STL.cpp
@@ -358,5 +358,50 @@ int main(){
 
 // ALGORITHMS 
 
+ bool comp(pair<int, int>p1, pair<int, int> p2){
+   if(p1.seocnd <p2.second) return true;
+   if(p1.second> p2.second) return false;
+   //they are same 
+   if(p1.first>p2.first) return true;
+   return false;
+}
+
+void explainExtra(){
+   sort(a, a+n );
+   sort(v.begin(), v.end());
+
+   sort(a+2, a+4);
+   sort(a, a+n, greater<int>);
+
+   pair<int,int> a[ ] = {{1,2}, {2,1}, {4,1}};
+
+   //sort it according to second element
+   ..if seocnd elemt is same then sort
+   it according to first elemet but in descending 
+
+   sort(a, a+n, comp);
+   //{4,1}, {2,1}, {1,2};
+
+   int num= 7;
+   int cnt= _builtin_popcount();
+   long long num = 12345678987654;
+   int cnt= _builtin_popcountll();
+
+   string s= "123";
+   do{
+      cout <<s<<endl;
+   
+   }while(next_permutation(s.begin(),s.end()));
+
+   int maxi = *max_element(a,a+n);
+
+}
+
+int main(){
+   return 0;
+}
+
+
  
+     
      
